95f48247f2c5e62cc3209f89dd5ea9417596997c
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-21 18:32:41 -0500

    added CRUD methods for graphql

---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretshhh';
+const expiration = '2h';
+
+module.exports = {
+    signToken: function({username, email, _id}) {
+        const payload = {username, email, _id};
+        return jwt.sign({data: payload}, secret, {expiresIn: expiration});
+    },
+    authMiddleware: function({req}) {
+        //alows token to be sent via req.body, req.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        //separate 'bearer' from '<tokenvalue>'
+        if (req.headers.authorization) {
+            token = token.split(' ')
+                        .pop()
+                        .trim();
+        }
+        //if no token, return request obj as is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            //decode and attach user data to request object
+            const {data} = jwt.verify(token, secret, {maxAge: expiration});
+            req.user = data;
+        } catch {
+            console.log('invalid token');
+        }
+        //return updated request object
+        return req;
+    }
+};
