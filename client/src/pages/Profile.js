f80e5a32ac014ef8a12e84c0c9b707a42dac72d0
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-27 17:35:41 -0500

    added form submission

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,9 +4,10 @@ import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER, QUERY_ME, ADD_FRIEND } from '../utils/queries';
 import Auth from '../utils/auth';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
@@ -14,6 +15,19 @@ const Profile = (props) => {
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME , {
     variables: { username: userParam },
   });
+  //destructure mutation function to use in click function
+  const [addFriend] = useMutation(ADD_FRIEND);
+  //on add button click, add friend to friends array of logged in user by id
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: {id: user._id}
+      });
+    } catch(e) {
+      console.error(e)
+    }
+  };
+
 
   const user = data?.me || data?.user || {};
 
@@ -41,6 +55,13 @@ if (!user?.username) {
           {/*if userParam doesnt exist, display viewing your profile*/}
           Viewing {userParam ? `${user.username}'s `: 'your'} 's profile.
         </h2>
+        {/*only show add friend button when url = `/profile/XXXXXXX`*/}
+        {userParam && (
+            <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -59,6 +80,7 @@ if (!user?.username) {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm/>}</div>
     </div>
   );
 };
