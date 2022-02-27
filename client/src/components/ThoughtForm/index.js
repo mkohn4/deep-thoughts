306cd046cf2fbe7b2cc472b9b270d3d100e3e018
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-27 17:51:51 -0500

    added thought mutation and logic

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,9 +1,38 @@
 import React, {useState} from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
 
     const [thoughtText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+
+    const [addThought, {error}] = useMutation(ADD_THOUGHT, {
+        update(cache, {data: {addThought}}) {
+
+            try {
+                //could potentially not exist yet so wrap in a try catch
+                //read whats currently in cache
+                const {thoughts} = cache.readQuery({query: QUERY_THOUGHTS});
+
+                //prepend the newest thought tot he front of the array
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: {thoughts: [addThought, ...thoughts]}
+                });
+            } catch(e) {
+                console.error(e);
+            }
+            
+            //update me object's cache, appending new thought tot he end of the array
+            const {me} = cache.readQuery({query: QUERY_ME});
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: {me: {...me, thoughts: [...me.thoughts, addThought]}}
+            });
+        }
+    });
 
     const handleChange = (e) => {
         if (e.target.value.length <=280) {
@@ -15,14 +44,27 @@ const ThoughtForm = () => {
 
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
-    }
+
+        try {
+            //add thought to database
+            await addThought({
+                variables: {thoughtText}
+            });
+
+            //clear form value
+            setText('');
+            setCharacterCount(0);
+        } catch(e) {
+            console.error(e);
+        }
+
+    };
 
     return(
         <div>
             <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/200
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form 
             className='flex-row justify-center justify-space-between-md align-stretch'
