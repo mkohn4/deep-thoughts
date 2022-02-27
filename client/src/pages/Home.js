df1aafb37a2ba162f9c436959f8a783dccfa511e
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-27 09:38:31 -0500

    added route and switch components based on url parameters

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,8 +14,8 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{
-          loading? (
+        <div className='col-12 mb-3'>
+          {loading ? (
             <div>Loading...</div>
           ) : (
             <ThoughtList thoughts={thoughts} title="Some feed for Thoughts..." />
