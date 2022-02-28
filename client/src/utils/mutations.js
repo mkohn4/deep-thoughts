69ca8f3985efda2bf191c1bf46876e24f383af3d
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-27 20:19:20 -0500

    added reaction form and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -52,3 +52,18 @@ mutation addThought($thoughtText: String!) {
     }
 }
 `;
+
+export const ADD_REACTION = gql`
+mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thooughtId, reactionBody: $reactionBody) {
+        _id
+        reactionCount
+        reactions {
+            _id
+            reactionBody
+            createdAt
+            username
+        }
+    }
+}
+`;
